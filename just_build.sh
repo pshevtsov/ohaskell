657a07d0847605b34640a458f4c04831fd90e0fc
Denis Shevchenko <me@dshevchenko.biz>
2015-07-16 14:46:44 +0300

    Переход на stack.

--- just_build.sh
+++ just_build.sh
@@ -2,10 +2,8 @@
 
 # Просто собираем веб-версию книги, локально.
 
-set -e
+stack install
 
-cabal clean && cabal configure && cabal build
-
-./dist/build/book/book rebuild
+book rebuild
 
 # После этого в корне репозитория смотрим в каталог _site.
