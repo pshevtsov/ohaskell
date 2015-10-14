ab0992d81e0b48f8093b72857a29cd26ad59de47
Denis Shevchenko <me@dshevchenko.biz>
2015-10-14 19:01:47 +0400

    Увеличение шрифта оглавления.

---
+++ _site/README.md
@@ -0,0 +1,5 @@
+# О Haskell по-человечески
+
+Книга о прекрасном и удивительном языке программирования Haskell. Для обыкновенных программистов.
+
+Создана с помощью блистательных [Hakyll](http://jaspervdj.be/hakyll/) и [Materialize](http://materializecss.com/).
---
+++ _site/static/js/default.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.button-collapse').sideNav({
+            menuWidth: 250, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    );
+})
